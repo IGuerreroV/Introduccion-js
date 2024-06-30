d51ddddb5501feb45f57782e830960ba7829563e
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-06-29 22:40:49 -0500

    Variables con let y const

---
+++ js/02.js
@@ -0,0 +1,25 @@
+// Variables con let
+
+let producto = 'Audifonos Gamer' // Iniciar variable y asignarle valor
+
+let disponible // Iniciamos variable sin valor
+
+// producto = true Reasignando el valor de la variable
+
+disponible = true
+
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'
+
+
+// let 1disponible | No puede iniciar con numeros
+let disponible_
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD' // Underscore
+let nombreProducto = 'Monitor HD' // Camelcase
+let NombreProducto = 'Monitor HD' // Pascal Case
+let nombreproducto = 'Monitor HD'
+
---
+++ js/03.js
@@ -0,0 +1,25 @@
+// Variables con const
+
+const producto = 'Audifonos Gamer' // Iniciar variable y asignarle valor
+
+let disponible = true // Iniciamos variable sin valor
+
+// producto = true Reasignando el valor de la variable
+
+disponible = true
+
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'
+
+
+// const 1disponible | No puede iniciar con numeros
+const disponible_ = true
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD' // Underscore
+const nombreProducto = 'Monitor HD' // Camelcase
+const NombreProducto = 'Monitor HD' // Pascal Case
+const nombreproducto = 'Monitor HD'
+
