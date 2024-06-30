9f5f0034c5b8a0289c45854119b90ca92532975b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-06-30 18:31:17 -0500

    Objetos

---
+++ js/10.js
@@ -0,0 +1,16 @@
+// Objetos
+
+
+const producto = {
+    nombreProducto: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible : true
+}
+
+console.log(producto);
+
+console.log(producto.precio);
+console.log(producto.disponible);
+console.log(producto.nombreProducto);
+
+console.log(producto['precio']);
