0ae813c7b50c80070f1d80df7bddc3ff8e65dca5
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-06-30 18:15:54 -0500

    Concatenación y Template Strings

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'Ivan'
+const email = 'correo@correo.com'
+
+// Concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+
+// Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
