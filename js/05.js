90b6e13837fbd3368a84be6e4538e2f4da54a7d9
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-06-29 23:54:34 -0500

    Números, operadores y el objeto Math en JS

---
+++ js/05.js
@@ -0,0 +1,15 @@
+// Numeros
+
+const numero1 = 100
+const numero2 = 200
+const numero3 = 7
+const numero4 = 6
+const numero5 = -5
+
+
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero3 % numero4);
---
+++ js/06.js
@@ -0,0 +1,17 @@
+// Objeto Math
+
+let resultado
+
+resultado = Math.PI
+resultado = Math.round(2.7)
+resultado = Math.ceil(2.2) // Ceil redondea gacia arriba
+resultado = Math.floor(2.9) // Floor redondea hacia abajo
+resultado = Math.sqrt(144)
+resultado = Math.abs(-200)
+resultado = Math.min(3, 5, 1, 8, 2, 10)
+resultado = Math.max(3, 5, 1, 8, 2, 10)
+resultado = Math.random()
+resultado = Math.floor(Math.random() * 30)
+
+
+console.log(resultado);
