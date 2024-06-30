7fd71cfd88fa675d9ca9d214c5286bab9024e847
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-06-30 18:07:17 -0500

    Controlar el orden de las operaciones

---
+++ js/07.js
@@ -0,0 +1,26 @@
+// Orden de operaciones
+
+let resultado
+
+resultado = (20 + 30) * 2
+
+resultado = (600 + 600) * 1.05
+
+
+// console.log(resultado);
+
+// Incrementos
+
+let puntaje = 10
+
+// Incemento en 1
+puntaje++ // 10
+++puntaje // 12
+
+puntaje += 10
+
+// Decremento en 1
+/* puntaje--
+--puntaje */
+
+console.log(puntaje);
