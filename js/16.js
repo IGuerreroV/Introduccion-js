49dc0e0d78a40e945487c371201e6d5e859fc051
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-04 20:38:26 -0500

    Explicando diferencias entre tipos de funciones y el hoisting

--- js/16.js
+++ js/16.js
@@ -1,16 +1,18 @@
 // Declaracion de Funcion
+sumar();
 function sumar() {
     console.log(10 + 10);
 }
 
-sumar();
+
 
 // Expresion de la funcion
+sumar2();
 const sumar2 = function() {
     console.log(3 + 3);
 }
 
-sumar2();
+
 
 // IIFE
 (function() {
