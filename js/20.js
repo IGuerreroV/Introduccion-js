b4461deca69bc9b7c2368c47abead1ede036631b
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-04 21:50:17 -0500

    Métodos de Propiedad

---
+++ js/20.js
@@ -0,0 +1,25 @@
+// Metodos de propiedad
+const reproductor = {
+    reproducir: function(id) {
+        console.log(`Reproduciendo cancion con el ID: ${id}`);
+    },
+    pausar: function() {
+        console.log('Pausando...');
+    },
+    crearPlayList: function(nombre) {
+        console.log(`Creando la playlist: ${nombre}`);
+    },
+    reproducirPlayList: function(nombre) {
+        console.log(`Reproduciendo la playlist: ${nombre}`);
+    }
+}
+
+reproductor.borrarCancion = function(id) {
+    console.log(`Eliminando la cancion: ${id}`);
+}
+
+reproductor.reproducir(3840)
+reproductor.pausar()
+reproductor.borrarCancion(20)
+reproductor.crearPlayList('Heavy Metal')
+reproductor.reproducirPlayList('Heavy Metal')
